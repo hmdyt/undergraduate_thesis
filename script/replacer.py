7574f9ba581f9a7aedf716a175fb8ea588617bbb
hmdyt <konnyaku108@gmail.com>
2022-04-04 20:11:34 +0900

    replace kutoten

--- script/replacer.py
+++ script/replacer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import glob
 
 def replace_str(file_path: str, replaced: str, replacing: str) -> None:
     with open(file_path, 'r') as f:
@@ -10,6 +11,15 @@ def replace_str(file_path: str, replaced: str, replacing: str) -> None:
         f.write(post_str)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("tar_file_path", type=str, help="target file path")
-parser.add_argument("str_replaced", type=str, help="a string replaced")
-parser.add_argument("str_replacing", type=str, help="a string replacing")
+parser.add_argument("--tar_file_path", type=str, help="target file path")
+parser.add_argument("--str_replaced", type=str, help="a string replaced")
+parser.add_argument("--str_replacing", type=str, help="a string replacing")
+parser.add_argument('--all', action='store_true')
+args = parser.parse_args()
+
+if args.all:
+    for a, b in [('、', ', '), ('。', '.')]:
+        for p in glob.glob("tex/*"):
+            replace_str(p, a, b)
+else:
+    replace_str(args.tar_file_path, args.str_replaced, args.str_replacing)
